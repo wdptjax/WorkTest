c058c39291e0c38aca724234cf36ef5c16cfbe83
wudepeng <wdhcc@163.com>
2018-06-13 15:42:28 +0800

    测试求最大值的效率（使用for循环与使用lambda表达式），结果for循环胜

--- DecenTest/Test/Program.cs
+++ DecenTest/Test/Program.cs
@@ -9,6 +9,38 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            //Test();
+        }
+
+        private static void Test()
+        {
+            int[] fl = new int[10000];
+            Random rd = new Random();
+            for (int i = 0; i < fl.Length; i++)
+            {
+                fl[i] = rd.Next();
+            }
+            while (string.IsNullOrEmpty(Console.ReadLine()))
+            {
+
+                DateTime dt = DateTime.Now;
+                for (int i = 0; i < 10000; i++)
+                {
+                    int max = fl.Max();
+                }
+                Console.WriteLine("lambda " + DateTime.Now.Subtract(dt).TotalMilliseconds.ToString("0.000"));
+                dt = DateTime.Now;
+                for (int i = 0; i < 10000; i++)
+                {
+                    int max = fl[0];
+                    for (int j = 0; j < fl.Length; j++)
+                    {
+                        if (max < fl[j])
+                            max = fl[j];
+                    }
+                }
+                Console.WriteLine("For " + DateTime.Now.Subtract(dt).TotalMilliseconds.ToString("0.000"));
+            }
         }
     }
 }
